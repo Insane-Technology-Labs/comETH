e2ec33e9ab8935cf60ddab94a4200fee5c5a5588
keccakdog <keccakdog@gmail.com>
2024-09-09 10:48:36 -0700

    ..

--- src/Bribable.sol
+++ src/Bribable.sol
@@ -3,23 +3,22 @@ pragma solidity ^0.8.0;
 
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {IWrappedTokenGatewayV3} from "./IWrappedTokenGatewayV3.sol";
+import {IAToken} from "./IAToken.sol";
 import {IRewarder} from "./IRewarder.sol";
 
 contract Bribable {
     /// @notice multichain multisig address
     address public constant OPERATIONS =
         0xBFc57B070b1EDA0FCb9c203EDc1085c626F3A36d;
-    address public underlying;
+    IERC20 public underlying;
     IRewarder public incentivesRecipient;
     IWrappedTokenGatewayV3 public wtg;
-    IERC20 public aToken;
+    IAToken public aToken;
     uint256 public constant DENOM = 10_000;
     uint256 public shareU = 5000;
     uint256 public shareT = 5000;
 
     uint8 internal initialized = 1;
-
-    bool public feeSwitch;
 
     modifier onlyOperator() {
         require(msg.sender == OPERATIONS, "unauthorized");
@@ -27,22 +26,16 @@ contract Bribable {
     }
 
     function init(
-        address _underlying,
         address _aToken,
         address _incentivesRecipient,
         address _gateway
     ) external onlyOperator {
         require(initialized == 1, "initialized");
-        aToken = IERC20(_aToken);
+        aToken = IAToken(_aToken);
         wtg = IWrappedTokenGatewayV3(_gateway);
         incentivesRecipient = IRewarder(_incentivesRecipient);
-        underlying = _underlying;
+        underlying = IERC20(aToken.UNDERLYING_ASSET_ADDRESS());
         ++initialized;
-        feeSwitch = false;
-    }
-
-    function toggle() external onlyOperator {
-        feeSwitch = !feeSwitch;
     }
 
     function changeRecipient(address _ir) external onlyOperator {
@@ -59,17 +52,18 @@ contract Bribable {
 
     function _hypothecate() internal {
         /// @dev poke logic
+
         uint256 before = aToken.balanceOf(address(this));
         aToken.transfer(OPERATIONS, 1 wei);
         uint256 dif = aToken.balanceOf(address(this)) - before;
-        if (feeSwitch) {
-            uint256 difU = ((dif * shareU) / DENOM);
-            uint256 difT = dif - difU;
-            aToken.transfer(OPERATIONS, difT);
-            IERC20(underlying).approve(address(incentivesRecipient), difU);
-            incentivesRecipient.notifyRewardAmountNext(underlying, difU);
-        } else {
-            aToken.transfer(OPERATIONS, dif);
-        }
+
+        /// @dev fee logic
+
+        uint256 difU = ((dif * shareU) / DENOM);
+        uint256 difT = dif - difU;
+        aToken.transfer(OPERATIONS, difT);
+        underlying.approve(address(incentivesRecipient), difU);
+        /// @dev lp bribe next epoch
+        incentivesRecipient.notifyRewardAmountNext(address(underlying), difU);
     }
 }
